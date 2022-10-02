c5287688c382c70ba2e45470170418b7cc6ed950
ketans2141 <105305997+ketans2141@users.noreply.github.com>
2022-10-02 16:03:31 +0530

    Fetch Covid Data Test

---
+++ covid-data-test/src/App.js
@@ -0,0 +1,13 @@
+import { FetchCovidData } from './components/FetchCovidData';
+// import logo from './logo.svg';
+// import './App.css';
+
+function App() {
+  return (
+    <>
+      <FetchCovidData/>
+    </>
+  );
+}
+
+export default App;
---
+++ covid-data-test/src/components/FetchCovidData.jsx
@@ -0,0 +1,86 @@
+import React,{useState,useEffect} from "react";
+import "./FetchCovidData.css"
+import axios from "axios"
+
+export function FetchCovidData(){
+
+    const [covidData,setCovidData]=useState([]);
+    const [search,setSearch] =useState("");
+    const [filterSearchData,setFilterSearchData]=useState([]);
+
+    useEffect(()=>{
+        async function getData(){
+            const response = await axios.get("https://data.covid19india.org/data.json")
+            setCovidData(response.data.statewise);
+            setFilterSearchData([covidData,...response.data.statewise])
+        }
+        //  let res = axios.get("https://data.covid19india.org/data.json")
+        //     .then(response=>{
+        //         console.log(response.data.statewise);
+        //         setCovidData(res.data.statewise)
+        //     })
+        getData();
+    },[])
+
+    useEffect(()=>{
+        const filterSearch=covidData.filter((item)=>{
+            if(item.state){
+                return item.state.includes(search)
+            }
+        });
+        
+        setFilterSearchData(filterSearch)
+    },[search]);
+
+    return(
+        <>
+            <div className="container">
+                <div className="main_heading">
+                    <img src="https://ukneqas.org.uk/wp-content/uploads/2020/03/Covid-19.png" alt="" className="covid_png"/>
+                    <h1 className="heading">India's Covid 19 Status</h1>
+                </div>
+
+                <div className="search_div">
+                    <input type="text" value={search} onChange={(e)=>setSearch(e.target.value)} placeholder="Enter your state..." className="search_input"/>
+                    <button className="search_btn">Search</button>
+                    <button className="dark_light">Dark / Light</button>
+                </div>
+                    <div>
+                        <table className="covid_data">
+                            <thead>
+                                <tr>
+                                    <th>State</th>
+                                    <th>Active</th>
+                                    <th>Confirmed</th>
+                                    <th>Recovered</th>
+                                    <th>Deaths</th>
+                                    <th>Updated Time</th>
+                                </tr>
+                            </thead>
+                            <tbody>
+                                {
+                                    filterSearchData.map((item,index)=>{
+                                        return(
+                                                    <>
+                                                        <tr key={index}>
+                                                            <td>{item.state}</td>
+                                                            <td>{item.active}</td>
+                                                            <td>{item.confirmed}</td>
+                                                            <td>{item.recovered}</td>
+                                                            <td className="death_col">{item.deaths}</td>
+                                                            <td>{item.lastupdatedtime}</td>
+                                                        </tr>
+                                                       
+                                                    </>
+                                                )
+                                        })
+                                }
+                            </tbody>
+                        </table>
+                    </div>
+               
+            </div>
+            
+        </>
+    )
+}
